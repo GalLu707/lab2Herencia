32cbd93d9129c5777e327a7239d91bae7b9f7335
GalLu707 <zasseuwu@gmail.com>
2026-01-30 08:53:39 -0600

    when haces tus momazos

--- src/lab2herencia/ClaseBaseEmpleado.java
+++ src/lab2herencia/ClaseBaseEmpleado.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lab2herencia;
-
-/**
- *
- * @author USER
- */
+import java.util.Calendar;
+        
+        
+        
 public class ClaseBaseEmpleado {
+    
+    protected int codigo;
+    protected String nombre;
+    protected Calendar fechaContratacion;
+    protected double salarioBase;
+    protected int HorasTrabajadas;
+    protected String tipo;
+    
+    public ClaseBaseEmpleado(int codigo, String nombre){
+        this.codigo= codigo;
+        this.nombre = nombre;
+        fechaContratacion= Calendar.getInstance();
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
     
 }
