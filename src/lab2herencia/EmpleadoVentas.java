1f0c6fc29c55797ab72b0441313ab1269ba525bf
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2026-01-30 09:22:33 -0600

    ventas

--- src/lab2herencia/EmpleadoVentas.java
+++ src/lab2herencia/EmpleadoVentas.java
@@ -8,7 +8,46 @@ package lab2herencia;
  *
  * @author USER
  */
-public class EmpleadoVentas {
+public class EmpleadoVentas extends ClaseBaseEmpleado {
 
-private double[]    
+private  double[]VentasAlMes;
+private  double TarifaComision;
+ 
+    public EmpleadoVentas(int codigo, String nombre, double TarifaComision) {
+        super(codigo, nombre);
+        
+        super.tipo="Ventas";
+     this.TarifaComision=TarifaComision;
+     VentasAlMes=new double[12];
+    }
+    
+     public void IngresarVentas(double CantidadVentas, int Mes){
+     VentasAlMes[Mes]+=CantidadVentas;
+     System.out.println("Registro de ventas exitosamente");
+     
+ }
+ 
+ public double EvaluarComision(int Mes){
+     double comision=VentasAlMes[Mes]*TarifaComision;
+     return comision;
+     
+ }
+ 
+ public double EvaluarSuledoMensual(int Mes){
+    double SueldoMensual= super.calcularPago()+EvaluarComision(Mes);
+    return SueldoMensual;
+    
+ }
+ 
+ public double EvaluarVentasAnuales(){
+     double TotalidadVentas=0;
+     for(int i=0; i<VentasAlMes.length; i++){
+      TotalidadVentas+=VentasAlMes[i];
+     }
+     return TotalidadVentas;
+ }
+ public String toStrin(){
+    String  Reporte= super.toString() +"\n Totalidad de ventaas en el año:"+EvaluarVentasAnuales();
+    return Reporte;
+ }
 }
