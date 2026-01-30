4d9e677a764490a6b81c95451adcba5d8019e749
GalLu707 <zasseuwu@gmail.com>
2026-01-30 08:29:21 -0600

    puse hola y las clases

--- src/lab2herencia/Lab2Herencia.java
+++ src/lab2herencia/Lab2Herencia.java
@@ -10,11 +10,9 @@ package lab2herencia;
  */
 public class Lab2Herencia {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
